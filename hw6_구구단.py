824f66fd3e5f3defe5c79a2255672a06fdb3f849
leenayeon03 <jerry1256@naver.com>
2025-04-20 21:17:36 +0900

    Add files via upload

--- hw6_구구단.py
+++ hw6_구구단.py
@@ -3,7 +3,7 @@
 def display_multiplication_table(n):
     for i in range(1,10):
         for j in range(n, n+4):
-            print(f'{j} x {i} = {j*i}', end='\t')
+            print(f' {j} x {i} = {j*i:2d}', end='\t')
         print()
     print()
     
